1bf1b4f53d109cba3cde73178bac285ae4e659f5
HyeonSeong <xkdtndua15@gmail.com>
2022-03-08 10:55:12 +0900

    note view model

---
+++ note_calendar/lib/presentation/note/2. add_edit_note/state/add_edit_note_state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'add_edit_note_state.freezed.dart';
+
+@freezed
+class AddEditNoteState with _$AddEditNoteState {
+  factory AddEditNoteState({
+    String? title,
+    String? content,
+    required int color,
+    required int fontSize,
+  }) = _AddEditNoteState;
+}
